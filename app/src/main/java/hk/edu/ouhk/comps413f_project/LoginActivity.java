7b8d722c2ed5f07d71f854fb59382ec7148bd29a
paul <ngwaipang381@gmail.com>
2021-11-27 17:49:30 +0800

    login change

---
+++ app/src/main/java/hk/edu/ouhk/comps413f_project/LoginActivity.java
@@ -0,0 +1,15 @@
+package hk.edu.ouhk.comps413f_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/hk/edu/ouhk/comps413f_project/MainActivity.java
+++ app/src/main/java/hk/edu/ouhk/comps413f_project/MainActivity.java
@@ -2,13 +2,28 @@ package hk.edu.ouhk.comps413f_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button gobutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gobutton = findViewById(R.id.go);
+
+        gobutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+
+              }
+        });
     }
 }
