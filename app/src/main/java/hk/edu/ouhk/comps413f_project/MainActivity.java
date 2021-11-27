33215bea1f0ff7ae6addd0263ebb8690906e61ec
paul <ngwaipang381@gmail.com>
2021-11-27 14:52:49 +0800

    first commit

---
+++ app/src/main/java/hk/edu/ouhk/comps413f_project/MainActivity.java
@@ -0,0 +1,14 @@
+package hk.edu.ouhk.comps413f_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
