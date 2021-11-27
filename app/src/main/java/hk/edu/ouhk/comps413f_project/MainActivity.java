d4eef9c32f7825dba5389110f18fca41e9b3c9d9
chanwingyeung <95116551+chanwingyeung@users.noreply.github.com>
2021-11-27 18:09:31 +0800

    splash_screen

--- app/src/main/java/hk/edu/ouhk/comps413f_project/MainActivity.java
+++ app/src/main/java/hk/edu/ouhk/comps413f_project/MainActivity.java
@@ -1,14 +1,25 @@
 package hk.edu.ouhk.comps413f_project;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 400;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                intent homeIntent = new  intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeintent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
