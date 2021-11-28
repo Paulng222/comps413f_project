9deb1e8a0201ed1e741c27f36000db8b41cb9cc0
paul <ngwaipang381@gmail.com>
2021-11-28 14:36:27 +0800

    splash screen changed(added app name)

--- app/src/main/java/hk/edu/ouhk/comps413f_project/MainActivity.java
+++ app/src/main/java/hk/edu/ouhk/comps413f_project/MainActivity.java
@@ -4,16 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.content.Intent;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 4000;
+    TextView appTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        appTitle = findViewById(R.id.appTitle);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
