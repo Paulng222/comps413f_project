11d13bf6c34de93787c7b9026ed6d134af6999dd
paul <ngwaipang381@gmail.com>
2021-11-29 10:14:05 +0800

    login part edited

--- app/src/main/java/hk/edu/ouhk/comps413f_project/MainActivity.java
+++ app/src/main/java/hk/edu/ouhk/comps413f_project/MainActivity.java
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 3000;
     TextView appTitle;
 
     @Override
