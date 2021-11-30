9a0dea4bb73bdf14da0e04a77f39fa88a95f62fb
paul <ngwaipang381@gmail.com>
2021-11-30 09:56:49 +0800

    delete options menu comment

--- app/src/main/java/hk/edu/ouhk/comps413f_project/HomeActivity.java
+++ app/src/main/java/hk/edu/ouhk/comps413f_project/HomeActivity.java
@@ -46,7 +46,6 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
